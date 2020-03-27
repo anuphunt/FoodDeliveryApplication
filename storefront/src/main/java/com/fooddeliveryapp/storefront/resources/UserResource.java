7a379d2b0fa844ce1df80e9790073bbe3550540f
anuphunt <anuphunt1@gmail.com>
2020-03-27 17:03:19 -0500

    added registerUser and getUser

--- storefront/src/main/java/com/fooddeliveryapp/storefront/resources/UserResource.java
+++ storefront/src/main/java/com/fooddeliveryapp/storefront/resources/UserResource.java
@@ -6,11 +6,8 @@ import com.fooddeliveryapp.storefront.models.UsersInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +15,8 @@ import java.util.List;
 @Api(value = "All data regarding users")
 @RequestMapping("/users")
 public class UserResource {
+
+    List<User> users = new ArrayList<>();
 
     @Autowired
     private RestTemplate restTemplate;
@@ -34,6 +33,26 @@ public class UserResource {
         users.add(new User(115L, "Anup Dhakal", "srana@miu.edu", "253-234-234", "234 random street"));
 
         return new UsersInfo(users);
+    }
+
+    @ApiOperation("Register new user")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public User registerUser(@RequestBody User user){
+        users.add(user);
+        return user;
+    }
+
+    @ApiOperation("Get user by id")
+    @RequestMapping(value = "{id}", method= RequestMethod.GET)
+    public User getUser(@PathVariable Long id){
+        User user = null;
+
+        for(User u: users){
+            if(u.getUserId() == id){
+                user = u;
+            }
+        }
+        return user;
     }
 
 
