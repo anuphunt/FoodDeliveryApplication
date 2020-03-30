178ef76f40afd4e240c13b4a7f3f976c397e6cfb
sbr mgr <sbr.mgr1@gmail.com>
2020-03-30 14:38:19 -0500

    changes in UI

--- frontend-client/vue-src/src/components/Layout.vue
+++ frontend-client/vue-src/src/components/Layout.vue
@@ -1,68 +1,155 @@
 <template>
 	<div>
-		<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+		<header>
+			<div class="hd-top">
+				<div class="container">
+					<div class="hdtop-left pull-left">
+						<p>Welcome <a href="#">Guest !</a></p>
+						<p><i class="fa fa-phone"></i>Call to order 099-412035 / 9809496968</p>
+					</div>
+					<div class="top-links">
+						<button id="linkButton">Account <i class="fa fa-angle-down"></i></button>
+						<ul class="pull-right">
+							<li><a href="/">My Account</a></li>
+							<li><router-link to="/login">Login</router-link></li>
+							<li><router-link to="/signup">Register</router-link></li>
+						</ul>
+					</div>
+				</div><!-- container end -->
+			</div>
+			<div class="hd-bottom">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-8 col-sm-3 col-sm-3">
+							<div class="logo">
+								<a href="index.html"><img src="logo.png" alt="logo"></a>
+							</div>
+						</div>
+						<div class="col-md-7">
+							<div class="hd-search">
+								<form method="post" action="#">
+									<input type="text" placeholder="Search what you are looking for?">
+									<button type="submit"><i class="fa fa-search"></i></button>
+								</form>
+							</div>
+						</div>
+						<div class="col-sm-3 col-md-2">
+							<button class="hd-cart pull-right">
+								<span class="text">CART</span>
+								<span class="cart-count">10</span>
+							</button>
+						</div>
+					</div><!-- row end -->
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 col-md-1 mrgb-30"></div>
+					<div class="col-sm-12 col-md-10 mrgb-30"></div>
+					<div class="wsmenucontent overlapblackbg"></div>
+					<div class="wsmenuexpandermain slideRight">
+						<a id="navToggle" class="animated-arrow slideLeft"><span></span></a>
+					</div>
+				</div>
+			</div><!-- container end -->
+		</header><!-- header end -->
+		<section class="main-body"><router-view/></section>
+		<footer>
+			<div class="foot-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4 col-md-3 mrgb-30">
+							<h3>Information</h3>
+							<ul>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Delivery Information</a></li>
+								<li><a href="#">Privacy and Policy</a></li> 
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">FAQ’s</a></li>
+								<li><a href="#">Blog</a></li>
+							</ul>
+						</div>						
+						<div class="col-sm-4 col-md-3 mrgb-30">
+							<h3>Customer Service</h3>
+							<ul>
+								<li><a href="#">Returns</a></li>
+								<li><a href="#">Site Map</a></li>
+								<li><a href="#">Order History</a></li> 
+								<li><a href="#">Other Services</a></li>
+								<li><a href="#">Sitemap</a></li>
+								<li><a href="#">Topography</a></li>
+							</ul>
+						</div>						
+						<div class="col-sm-4 col-md-3 mrgb-30">
+							<h3>My Account</h3>
+							<ul>
+								<li><a href="#">My Account</a></li>
+								<li><a href="#">Signin</a></li>
+								<li><a href="#">Signup</a></li> 
+								<li><a href="#">Cart</a></li>
+								<li><a href="#">Checkout</a></li>
+								<li><a href="#">Wishlist</a></li>
+							</ul>
+						</div>						
+						<div class="col-sm-12 col-md-3 mrgb-30">
+							<h3>Contact</h3>
+							<address class="mrgb-20">
+								<div class="foot-continfo mrgb-20">
+									<div class="icon-cont">
+										<i class="fa fa-paper-plane"></i>
+									</div>
+									<div class="add-info">
+										ur business address is 1063 Free ype and scrambled it to make
+									</div>    
+								</div>
+								<div class="foot-continfo mrgb-20">
+									<div class="icon-cont">
+										<i class="fa fa-phone-square"></i>
+									</div>
+									<div class="add-info">
+										+ 977- 010000000
+									</div>   
+								</div>
+								<div class="foot-continfo mrgb-20">
+									<div class="icon-cont">
+										<i class="fa fa-envelope"></i>
+									</div>    
+									<div class="add-info">
+										support@snapb.com
+									</div>    
+								</div>
+							</address>							
+							<ul class="social-link">
+								<li><a href="#" target="_blank"><img src="images/icons/fb.png" alt="fb"></a></li>
+								<li><a href="#" target="_blank"><img src="images/icons/youtube.png" alt="youtube"></a></li>
+								<li><a href="#" target="_blank"><img src="images/icons/gplus.png" alt="gplus"></a></li>
+								<li><a href="#" target="_blank"><img src="images/icons/instagram.png" alt="instagram"></a></li>
+							</ul>
+						</div>
+						
+					</div>
+				</div>
+			</div><!-- footer top end -->			
+			<div class="foot-btm">
+				<div class="container">
+					<p>© 2019 LockDownFood.com | All Right Reserved.</p>
+				</div>
+			</div>
+			
+		</footer><!-- footer end -->
+		
 	</div>
 </template>
 <script>
 import './../assets/bootstrap-3.3.7-dist/css/bootstrap.min.css'
-import './../assets/jquery.js'
-import './../assets/bootstrap-3.3.7-dist/js/bootstrap.min.js'
+import './../assets/src/css/menu.css'
+import './../assets/src/css/style.css'
+import './../assets/src/css/font-awesome.min.css'
 export default {
 	name: 'Layout',
 	props: {
 		msg: String
-	}
+	},
+
 }
 </script>
--- frontend-client/vue-src/src/components/Signup.vue
+++ frontend-client/vue-src/src/components/Signup.vue
@@ -1,11 +1,52 @@
 <template>
-<p>Sign vue</p>
+<div class="container">
+      <div class="row">
+      <div class="col-sm-12 col-md-12 mrgb-30">
+                <div class="heading-all mrgb-20">
+                  <div class="clearfix">
+                        <h1>Register</h1>
+                    </div>
+                    <div class="hding-sept"></div>
+                </div>
+                <form method="post" action="./">
+                    <div class="row">
+                      <div class="col-sm-6 col-md-6">
+                        <div class="clearfix mrgb-30">
+                          <h3 class="mrgb-20">Personal Information</h3>                            
+                            <label for="firstName">First Name</label>
+                            <input type="text" name="firstName" class="form-control mrgt-10 mrgb-10"/>
+                          
+                            <label for="lastName">Last Name</label>
+                            <input type="text" name="lastName" class="form-control mrgt-10 mrgb-10"/>
+                            
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control mrgt-10 mrgb-10"/>
+                            
+                            <label>Teliphone</label>
+                            <input type="text" name="teliphone" class="form-control mrgt-10 mrgb-10"/>
+                          
+                          <hr/>
+                          <label>Password</label>
+                          <input type="password" name="password" class="form-control mrgt-10 mrgb-10"/>
+                          
+                          <label>Password Confirm</label>
+                          <input type="password" name="confirmpassword" class="form-control mrgt-10 mrgb-10"/>
+                          
+                          <hr/>
+                          <button class="btn-all pull-right">Register</button>
+                        </div>
+                      </div>
+                    </div><!-- row end -->   
+                </form>
+           </div>
+        </div>
+       </div>
 </template>
 <script type="text/javascript">
 export default {
-	name: 'Signup',
-	props: {
-		msg: String
-	}
+    name: 'Signup',
+    props: {
+        msg: String
+    }
 }
 </script>
