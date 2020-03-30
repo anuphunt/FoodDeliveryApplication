db19f385a640d77ddf20de107c9ef49f97019556
sbr mgr <sbr.mgr1@gmail.com>
2020-03-30 16:42:18 -0500

    added form validation for signup

--- frontend-client/vue-src/src/components/Signup.vue
+++ frontend-client/vue-src/src/components/Signup.vue
@@ -8,29 +8,35 @@
                     </div>
                     <div class="hding-sept"></div>
                 </div>
-                <form method="post" action="./">
-                    <div class="row">
+                <form @submit="checkForm" method="post" action="./">
+                     <p v-if="errors.length" class="text-danger">
+                        <b>Please correct the following error(s):</b>
+                        <ul>
+                          <li v-for="error in errors"   :key="error.key">{{ error.value }}</li>
+                        </ul>
+                      </p>
+                      <div class="row">
                       <div class="col-sm-6 col-md-6">
                         <div class="clearfix mrgb-30">
                           <h3 class="mrgb-20">Personal Information</h3>                            
                             <label for="firstName">First Name</label>
-                            <input type="text" name="firstName" class="form-control mrgt-10 mrgb-10"/>
+                            <input type="text" v-model="firstName" name="firstName" class="form-control mrgt-10 mrgb-10"/>
                           
                             <label for="lastName">Last Name</label>
-                            <input type="text" name="lastName" class="form-control mrgt-10 mrgb-10"/>
+                            <input type="text" v-model="lastName"  name="lastName" class="form-control mrgt-10 mrgb-10"/>
                             
                             <label>Email</label>
-                            <input type="text" name="email" class="form-control mrgt-10 mrgb-10"/>
+                            <input type="text" v-model="email" name="email" class="form-control mrgt-10 mrgb-10"/>
                             
                             <label>Teliphone</label>
-                            <input type="text" name="teliphone" class="form-control mrgt-10 mrgb-10"/>
+                            <input type="text"  v-model="phone"  name="phone" class="form-control mrgt-10 mrgb-10"/>
                           
                           <hr/>
                           <label>Password</label>
-                          <input type="password" name="password" class="form-control mrgt-10 mrgb-10"/>
+                          <input type="password"  v-model="password" name="password" class="form-control mrgt-10 mrgb-10"/>
                           
                           <label>Password Confirm</label>
-                          <input type="password" name="confirmpassword" class="form-control mrgt-10 mrgb-10"/>
+                          <input type="password"   v-model="confirmPassword" name="confirmPassword" class="form-control mrgt-10 mrgb-10"/>
                           
                           <hr/>
                           <button class="btn-all pull-right">Register</button>
@@ -45,8 +51,66 @@
 <script type="text/javascript">
 export default {
     name: 'Signup',
-    props: {
-        msg: String
+    data() {
+        return {
+          errors:[],
+          firstName:null,
+          lastName:null,
+          email:null,
+          phone:null,
+          password:null,
+          confirmPassword:null
+        }
+    },
+    methods:{
+    checkForm: function (e) {
+
+      if (this.firstName && this.lastName && this.email && this.phone && this.password && this.confirmPassword) {
+            this.axios.post('google.com').then((response)=>{
+                alert('requested');
+                console.log(response);
+            })
+            .catch((errors)=>{
+                console.log(errors);
+
+            })
+      }
+
+      this.errors = [];
+      var i = 0;
+
+      if (!this.firstName) {
+        this.errors.push({
+            key:i++,
+            value:'First name is required'
+        });
+      }
+      if (!this.lastName) {
+        this.errors.push({
+            key:i++,
+            value:'last name is required'
+        });
+      }
+      if (!this.email) {
+        this.errors.push({
+            key:i++,
+            value:'Email is required'
+        });
+      }
+      if (!this.password) {
+        this.errors.push({
+            key:i++,
+            value:'Password is required'
+        });
+      }
+      if (!this.confirmPassword) {
+        this.errors.push({
+            key:i++,
+            value:'Confirm Password is required'
+        });
+      }
+      e.preventDefault();
     }
+}
 }
 </script>
