c46b28d9c30913b7f0b8223be2ed4191b1a63c10
anuphunt <anuphunt1@gmail.com>
2020-04-01 18:27:35 -0500

    added cors policy access to all controllers

--- ApiGateway/storefront/src/main/java/com/fooddeliveryapp/storefront/resources/FoodItemResource.java
+++ ApiGateway/storefront/src/main/java/com/fooddeliveryapp/storefront/resources/FoodItemResource.java
@@ -5,16 +5,14 @@ import com.fooddeliveryapp.storefront.models.FoodItem;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Api(value = "All data regarding foods")
 @RequestMapping("/foods")
 public class FoodItemResource {
