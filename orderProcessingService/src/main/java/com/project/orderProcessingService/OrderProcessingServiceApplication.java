1f311d739baa5bb86b9f69fe3e21531ca9a2cfe1
paudelnavin <navinpaudel@outlook.com>
2020-04-02 15:24:02 -0500

    Added order model and controller

---
+++ orderProcessingService/src/main/java/com/project/orderProcessingService/OrderProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.orderProcessingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingServiceApplication.class, args);
+	}
+
+}
