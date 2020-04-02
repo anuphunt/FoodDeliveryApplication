386da6b4ab2a0072d1482461a8c9b32736553394
paudelnavin <navinpaudel@outlook.com>
2020-04-02 15:26:29 -0500

    managed order processing system

---
+++ orderProcessingService/src/test/java/com/project/orderProcessingService/OrderProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.orderProcessingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
