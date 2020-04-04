080f57ffcc58b8d8ca77862d58fac5b73a81eb77
sbr mgr <sbr.mgr1@gmail.com>
2020-04-04 16:10:24 -0500

    Restaurent food adding form is ready to api implementation

--- frontend-client/vue-src/src/components/ManageRestaurentFood.vue
+++ frontend-client/vue-src/src/components/ManageRestaurentFood.vue
@@ -1,32 +1,34 @@
 <template>
     <div class="container container-height">
-        <div class="row">
-          <RestaurentNav/>
-          <div class="form-group">
-            <router-link class="btn btn-success" to="/restaurent-add-food">Add New Food</router-link>
-          </div>
-          <div class="">
-            <table cellspacing="0" id="tech-companies-1" class="table table-small-font table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>Food Name</th>
-                  <th>Unit Price</th>
-                  <th>Image</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(food, index) in foods" :key="index">
-                  <td>{{food.name}}</td>
-                  <td>$100</td>
-                  <td><img height="120" v-bind:alt="food.name" src="/dummy-food.jpg"></td>
-                  <td width="120">
-                    <a href="" class="btn btn-xs btn-primary">Edit</a>
-                    &nbsp;<a href="" class="btn btn-xs btn-danger">Delete</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+        <div class="col-lg-12">
+          <div class="row">
+            <RestaurentNav/>
+            <div class="form-group">
+              <router-link class="btn btn-success" to="/restaurent-add-food">Add New Food</router-link>
+            </div>
+            <div class="">
+              <table cellspacing="0" id="tech-companies-1" class="table table-small-font table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Food Name</th>
+                    <th>Unit Price</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(food, index) in foods" :key="index">
+                    <td>{{food.name}}</td>
+                    <td>$100</td>
+                    <td><img height="120" v-bind:alt="food.name" src="/dummy-food.jpg"></td>
+                    <td width="120">
+                      <a href="" class="btn btn-xs btn-primary">Edit</a>
+                      &nbsp;<a href="" class="btn btn-xs btn-danger">Delete</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
     </div>
--- frontend-client/vue-src/src/router.js
+++ frontend-client/vue-src/src/router.js
@@ -14,6 +14,7 @@ import CartDetails from './components/CartDetails.vue'
 import RestaurentOrderList from './components/RestaurentOrderList.vue'
 import CantactUs from './components/contact-us.vue'
 import ManageRestaurentFood from './components/ManageRestaurentFood.vue'
+import RestaurentFoodForm from './components/RestaurentFoodForm.vue'
 
 Vue.use(Router)
 
@@ -91,6 +92,11 @@ export default new Router({
 			path:'/product-management',
 			name:'ManageRestaurentFood',
 			component:ManageRestaurentFood
+		},
+		{  
+			path:'/restaurent-add-food',
+			name:'RestaurentFoodForm',
+			component:RestaurentFoodForm
 		}
 	],
 	duplicateNavigationPolicy: 'reload'
