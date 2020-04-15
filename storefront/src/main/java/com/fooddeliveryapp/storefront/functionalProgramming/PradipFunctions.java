aace502dbda45e2207debad9c72cacc49f219ea9
pradipacharya7 <pradip.acharya077@gmail.com>
2020-04-14 22:57:36 -0500

    Two queries added with a tetrafunction interface

--- storefront/src/main/java/com/fooddeliveryapp/storefront/functionalProgramming/PradipFunctions.java
+++ storefront/src/main/java/com/fooddeliveryapp/storefront/functionalProgramming/PradipFunctions.java
@@ -9,17 +9,22 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class PradipFunctions {
-//    Get top ten food items ordered in last week and have rating more than 4
+
+    //    Get top ten food items ordered in last week and have rating more than 4
 
     public static BiFunction<List<Food>, LocalDate, List<Food>> highlyRatedFoods = (food, date) -> food.stream()
             .filter(f -> f.getRating() >= 4 && date.getYear() == LocalDate.now().getYear() && date.getMonth() == LocalDate.now().getMonth())
             .limit(2)
             .collect(Collectors.toList());
-//    Get all restaurants that have rating < 2 rated by users.
-// Get all restaurants that have at least X users rated them less than 2 but foods more than x
-    public static BiFunction<List<User>,Integer,List<User>> getAllRestaurants = (users,foodNumber) -> users.stream()
-            .filter(r -> r.getRating() <= 2 && r.getRole()==UserRole.RESTAURANT)
 
-            .limit(5)
+    // Get all restaurants that have rate less than 2 but foods more than x
+    public static TetraFunction<List<User>,List<Food>,Integer,Integer, List<User>> lowRatedRestaurants = (restaurants,foods,min_rate,no_foods)->foods
+        .stream()
+        .collect(Collectors.groupingBy(Food::getRestaurantId))
+        .entrySet()
+        .stream()
+        .filter((restaurantFoods)->restaurantFoods.getValue().size()>=no_foods)
+        .map((restaurantFoods)->restaurants.stream().filter(restaurant->restaurant.getId() == restaurantFoods.getKey()).findFirst().get())
+            .filter((res)->res.getRating()<=min_rate)
             .collect(Collectors.toList());
 }
